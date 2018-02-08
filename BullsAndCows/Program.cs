b7632cf238d956a5e7bf342098d45da3940ac16c
Алёна <lobankovaalyona@gmail.com>
2018-02-08 18:20:10 +0700

    Started to write WhatNumber Game

--- BullsAndCows/Program.cs
+++ BullsAndCows/Program.cs
@@ -11,28 +11,47 @@ namespace BullsAndCows
         static void Main(string[] args)
         {
             // игра быки и коровы.
+            var game = new Games();
+
             Console.WriteLine("Компьютер загадал четырехзначеное число. Вам необходимо угадать его за 10 попыток.");
             Console.WriteLine("Быки - нужные цифры на своих местах. Коровы - нужные цифры не на своих местах.\n");
 
             do
             {
-                int[] Que = GenerateNumber();
-                Games game = new Games();
-                bool g = game.Game(Que);
-                if (g == true)
-                    Console.WriteLine("Вы выиграли!");
-                else
-                    Console.WriteLine("Вы проиграли :(");
+                game.Start();
+                Console.WriteLine("\tПопытка\tВвод\tРезультат");
+                while (!game.IsLose())
+                {
+                    var guess = Int32.Parse(Console.ReadLine());
+                    game.Guess(guess, out int bulls, out int cows);
+
+                    Console.WriteLine($"\t{game.TriesCount}\t{guess}\tБ{bulls}К{cows - bulls}");
+
+                    if (game.IsWin(bulls) == true)
+                    {
+                        Console.WriteLine("Вы выиграли!");
+                        break;
+                    }
+
+                }
                 Console.WriteLine("Хотите сыграть еще раз? y/n");
             }
             while (Console.ReadLine() == "y");
+
             Console.WriteLine("Нажмите любую клавишу");
             Console.ReadKey();
         }
-        static int[] GenerateNumber() // генерация загадываемого числа
+    }
+    public class Games
+    {
+        private int[] Que = { 0, 0, 0, 0 };
+
+        public int TriesCount { get; private set; }
+
+        public void Start()
         {
+            TriesCount = 10;
             Random rnd = new Random();
-            int[] Que = { 0, 0, 0, 0 };
             int j = 0;
             Que[j] = rnd.Next(1, 10);
             for (j = 1; j < 4; j++)
@@ -40,49 +59,48 @@ namespace BullsAndCows
                 Que[j] = rnd.Next(1, 10);
                 for (int p = 0; p < 4; p++)
                 {
-                    if ((Que[j] == Que[p]) && (j != p)) { j--; break; } // числа не должны повторяться
+                    if ((Que[j] == Que[p]) && (j != p))
+                    {
+                        j--;
+                        break;
+                    } // числа не должны повторяться
                 }
             }
-            return Que;
         }
 
-    }
-    class Games
-    {
-        public bool Game(int[] Que)
+        public void Guess(int guess, out int bulls, out int cows)
         {
-            Console.WriteLine("\tПопытка\tВвод\tРезультат");
-            for (int i = 10; i > 0; i--)
+            cows = 0; bulls = 0; // коровы, быки
+
+            int[] num = { 0, 0, 0, 0 };
+            int j = 0;
+            num[j] = guess / 1000; // тысячные
+            num[j + 1] = (guess - num[j] * 1000) / 100; // сотни
+            num[j + 2] = (guess - num[j] * 1000 - num[j + 1] * 100) / 10; // десятки
+            num[j + 3] = (guess - num[j] * 1000 - num[j + 1] * 100 - num[j + 2] * 10); // единицы
+
+            for (j = 0; j < 4; j++)
             {
-                int num; //введенное пользователем число
-                //Console.WriteLine($"Попыток:{i}");
-                num = Int32.Parse(Console.ReadLine());
-
-                int[] Num = { 0, 0, 0, 0 };
-                int j = 0;
-                Num[j] = num / 1000; // тысячные
-                Num[j + 1] = (num - Num[j] * 1000) / 100; // сотни
-                Num[j + 2] = (num - Num[j] * 1000 - Num[j + 1] * 100) / 10; // десятки
-                Num[j + 3] = (num - Num[j] * 1000 - Num[j + 1] * 100 - Num[j + 2] * 10); // единицы
-
-                int cow = 0, bull = 0; // коровы, быки
-                for (j = 0; j < 4; j++)
+                for (int k = 0; k < 4; k++)
                 {
-                    for (int k = 0; k < 4; k++)
-                    {
-                        if (Num[j] == Que[k]) cow++;
+                    if (num[j] == Que[k]) cows++;
 
-                    }
-                    if (Num[j] == Que[j]) bull++;
                 }
-                //Console.WriteLine($"Б{bull}К{cow - bull}");
-                Console.WriteLine($"\t{i}\t{num}\tБ{bull}К{cow - bull}");
-                if (bull == 4)
-                {
-                    return true;
-                }
+                if (num[j] == Que[j]) bulls++;
             }
+
+            TriesCount--;
+        }
+
+        public bool IsWin(int bull)
+        {
+            if (bull == 4)
+                return true;
             return false;
+        }
+        public bool IsLose()
+        {
+            return TriesCount == 0;
         }
     }
 }
